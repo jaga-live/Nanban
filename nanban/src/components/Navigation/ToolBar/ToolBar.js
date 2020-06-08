7c95fc183519fbfd2d2011dc7d2c054b2c3e0067
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-06-08 09:58:50 +0530

    Responsive Navigation

--- nanban/src/components/Navigation/ToolBar/ToolBar.js
+++ nanban/src/components/Navigation/ToolBar/ToolBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 import './ToolBar.css'
 import ToggleButton from '../ToggleButton/ToggleButton'
+import mainLogo from'../../../images/mainLogo.png'
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => {
   return (
     <div className='Toolbar'>
       <ToggleButton onClick = {props.onClick} />
+      <div className="img">
+        <img src={mainLogo} />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
       <h3>Nanban Connect</h3>
     </div>
   )
